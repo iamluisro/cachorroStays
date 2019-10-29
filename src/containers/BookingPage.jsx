6992cd23bb2d6926f8776a5c529707a7b6ac9a5c
Agustin Moran <agustin.moranr@gmail.com>
2019-10-29 01:47:15 -0600

    Fixing bookingPage.jsx and Booking.html, also with its styles css & scss.

---
+++ src/containers/BookingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import BookingItem from '../components/BookingItem';
+
+const BookingPage = () => {
+  return (
+    <Layout>
+      <BookingItem />
+    </Layout>
+  );
+};
+
+export default BookingPage;
