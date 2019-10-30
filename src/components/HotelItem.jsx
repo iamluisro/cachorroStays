44ac37f95337502441ca072da03aefc9f7b2fd00
lfrodriguez <lfrodriguez06@gmail.com>
2019-10-30 00:36:26 -0600

    added Routes to the home page using App.js to be the hub for the routes in the app. I also modified a couple of CSS errors that popped up.

--- src/components/HotelItem.jsx
+++ src/components/HotelItem.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-tabs */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HotelImage from '../assets/img/hikaru.png';
 import '../assets/styles/home_styles.scss';
 
@@ -10,13 +12,19 @@ const HotelItem = () => {
           <img className='hotel__Container--img' src={HotelImage} alt='pethotel' />
           <h1 className='hotel__container--details--title'>Pension Para Mascotas Hikaru</h1>
           <p className='hotel__container--details--subdetails'>200MXN$ / noche</p>
-          <p className='hotel__container--details--subdetails'>
-            Sto Tomas Ajusco Km 28 1/2 Picacho Ajusco Tlalpan, Mexico
-          </p>
+          <p className='hotel__container--details--subdetails'>Sto Tomas Ajusco Km 28 1/2 Picacho Ajusco Tlalpan, Mexico</p>
           <div className='hotel__container--rating'>*****</div>
           <div className='hotel__container--buttons'>
-            <button type='button' className='hotel__tile--buttons'>Share</button>
-            <button type='button' className='hotel__tile--buttons'>Explore</button>
+            <button type='button' className='hotel__tile--buttons'>
+              {' '}
+							Share
+              {' '}
+            </button>
+            <Link to='/hotel-details'>
+              <button type='button' className='hotel__tile--buttons'>
+								Explore
+              </button>
+            </Link>
           </div>
         </div>
       </section>
--- src/containers/BookingPage.jsx
+++ src/containers/BookingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import BookingItem from '../components/BookingItem';
+import '../assets/styles/components/booking_styles.scss';
 
 const BookingPage = () => {
   return (
