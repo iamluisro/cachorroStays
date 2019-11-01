e39cf11dc8f5dc676bad5b1ad460014b501444a7
lfrodriguez <lfrodriguez06@gmail.com>
2019-10-31 21:14:49 -0600

    Added the utils folder and created the mockHotels.js file where we can store the mock data for now.

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import '../assets/styles/components/notFound_styles.scss';
-import dogNotFoundImg from '../assets/img/dog-notfound.png';
+//import dogNotFoundImg from '../assets/img/dog-notfound.png';
 
 const NotFound = () => {
   return (
@@ -9,11 +9,11 @@ const NotFound = () => {
       <div className='notFound'>
         <h1>404</h1>
         <h2>
-Page not found.
+					Page not found.
           <br />
-Lo sentimos, No pudimos encontrar dicho sitio.
+					Lo sentimos, No pudimos encontrar dicho sitio.
         </h2>
-        <img src={dogNotFoundImg} alt='Dog-swimming' />
+        {/*   <img src={dogNotFoundImg} alt='Dog-swimming' /> */}
       </div>
     </Layout>
   );
