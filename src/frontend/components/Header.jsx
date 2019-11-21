53e15f3627a5feedd7fac2c40f516e87e6591749
Agustin Moran <agustin.moranr@gmail.com>
2019-11-20 19:38:51 -0600

    Se modifico la parte del header, para implementar el boton para iniciar sesion y registrarse

--- src/frontend/components/Header.jsx
+++ src/frontend/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss';
+import dogBone from '../../assets/img/dogBone-icon.png';
 
 const Header = () => {
   return (
@@ -9,6 +10,22 @@ const Header = () => {
         <Link to='/'>
           <h1>CachorroStays</h1>
         </Link>
+
+        <div className='header__menu'>
+          <div className='header__menu--profile'>
+            <img src={dogBone} alt='' />
+            <p>Perfil</p>
+          </div>
+          <ul>
+            <Link to='/register'>
+              <li><a href='/'>Regístrate</a></li>
+            </Link>
+            <Link to='/login'>
+              <li><a href='/'>Iniciar Sesión</a></li>
+            </Link>
+          </ul>
+        </div>
+
       </header>
     </div>
   );
