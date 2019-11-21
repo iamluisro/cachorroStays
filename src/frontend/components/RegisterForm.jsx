aaa42082f38c99ae6ea632904ab78b488e35ecb4
lfrodriguez <lfrodriguez06@gmail.com>
2019-11-21 13:32:49 -0600

    added registerRequest action and reducer to register form

--- src/frontend/components/RegisterForm.jsx
+++ src/frontend/components/RegisterForm.jsx
@@ -1,28 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { registerRequest } from '../actions';
 
 const RegisterForm = () => {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault;
+    props.registerRequest(form);
+    props.history.push('/');
+    console.log(form);
+  };
+
   return (
-    <form className='register__container--form'>
-      <input className='inputForm' type='text' placeholder='First name' />
-      <input className='inputForm' type='text' placeholder='Last name' />
-      <input className='inputForm' type='text' placeholder='Username' />
-      <input className='inputForm' type='text' placeholder='Email' />
-      <input className='inputForm' type='text' placeholder='Phone Number' />
+    <form className='register__container--form' onSubmite={handleSubmit}>
+      <input
+        className='inputForm'
+        type='text'
+        placeholder='Name'
+        onChange={handleInput}
+      />
+
+      <input
+        className='inputForm'
+        type='text'
+        placeholder='Email'
+        onChange={handleInput}
+      />
       <input className='inputForm' type='password' placeholder='Password' />
-      <input className='inputForm' type='password' placeholder='Confirm Password' />
+      <input
+        className='inputForm'
+        type='password'
+        placeholder='Confirm Password'
+        onChange={handleInput}
+      />
       <Link to='/'>
-        <button type='button' className='registerButton'>Register-me</button>
+        <button type='button' className='registerButton'>
+          Register-me
+        </button>
       </Link>
       <div className='privacyTerms'>
-        <button type='button' className='privacyTerms__button' alt='TermsAndPolicy' />
-        <p className='privacyTerms__text'>I agree to terms and privicy policy</p>
+        <button
+          type='button'
+          className='privacyTerms__button'
+          alt='TermsAndPolicy'
+        />
+        <p className='privacyTerms__text'>
+          I agree to terms and privicy policy
+        </p>
       </div>
       <Link to='/login'>
-        <button type='button' className='signInButton'>Log in</button>
+        <button type='button' className='signInButton'>
+          Log in
+        </button>
       </Link>
     </form>
   );
 };
 
-export default RegisterForm;
+const mapDispatchToProps = {
+  registerRequest,
+};
+export default connect(null, mapDispatchToProps)(RegisterForm);
